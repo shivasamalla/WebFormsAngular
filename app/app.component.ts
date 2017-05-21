010eee356e6575b98f1b9ff01346782088c22c93
Iulian Paraian <iparaian@gmail.com>
2017-05-21 21:17:51 +0100

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  //template: '<h1>Hello {{name}}</h1>'
+  templateUrl: "./hello-world.html"
+})
+export class AppComponent { name = 'Angular'; }
+
